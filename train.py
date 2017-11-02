7290d896c29c5b80664c905562f19346284b2212
taowen <taowen0828@gmail.com>
2017-11-02 20:35:03 +0800

    modify lr and add checkpoint

--- train.py
+++ train.py
@@ -4,22 +4,26 @@ import numpy as np
 import tensorflow as tf
 from model import net
 
-import tensorflow.contrib.slim as slim
+# import tensorflow.contrib.slim as slim
 
 import sys
 sys.path.append("../../")
 
 from dataset2 import decoder_tfRecords
 
+checkpoint_directory = './checkpoints/'
+log_directory = '../../log/video_interpolation/'
+tfrecords_path = '../../Data/UCF101_dataset_float32.tfrecords'
+
 BATCH_SIZE = 32
-LEARNING_RATE = 0.0001
-MAX_EPOCHES = 100000000
+START_LEARNING_RATE = 0.001
+MAX_EPOCHES = 100000
 height = 128
 width = 128
 depth = 3
 
 
-frames, ground_truth = decoder_tfRecords('../../Data/UCF101_dataset_float32.tfrecords')
+frames, ground_truth = decoder_tfRecords(tfrecords_path)
 
 # frames = tf.image.resize_image_with_crop_or_pad(image=frames, target_height=height, target_width=width)
 # ground_truth = tf.image.resize_image_with_crop_or_pad(image=ground_truth, target_height=height, target_width=width)
@@ -35,9 +39,14 @@ img_batch, label_batch = tf.train.shuffle_batch(
 inputs = tf.placeholder(tf.float32, [None, height, width, depth*2])
 labels = tf.placeholder(tf.float32, [None, height, width, depth])
 
+global_steps = tf.Variable(0, dtype=tf.int32, trainable=False)
+saver = tf.train.Saver()
+
+learning_rate = tf.train.exponential_decay(START_LEARNING_RATE, global_steps, 5000, 0.96, staircase=True)
+
 loss_l2 = net(inputs, labels)
 tf.summary.scalar('loss', loss_l2)
-optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(loss_l2)
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss_l2, global_step=global_steps)
 
 for var in tf.trainable_variables():
     tf.summary.histogram(var.name, var)
@@ -50,9 +59,13 @@ with tf.Session() as sess:
     sess.run(init)
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(coord=coord)
-    writer = tf.summary.FileWriter('./log',sess.graph)
+    writer = tf.summary.FileWriter(log_directory, sess.graph)
     for i in range(MAX_EPOCHES):
         _, loss, summary = sess.run([optimizer, loss_l2, merged], feed_dict={inputs:img_batch.eval(), labels:label_batch.eval()})
         writer.add_summary(summary, i)
+
+        if (i+1) % 1000 == 0:
+            saver.save(sess, checkpoint_directory+'checkpoint_model_%s.model' % str(i+1), global_step=global_steps)
+
         print 'Iteration = %s, loss = %s' % (str(i), str(loss/BATCH_SIZE))
 writer.close()
