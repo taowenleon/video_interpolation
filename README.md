8cce343d8d8ee813ec4546ac69ca16b888697d6c
taowen <taowen0828@gmail.com>
2017-12-05 11:13:32 +0800

    add some results

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # video_interpolation
 video frame interpolation with deep learning
+
+This is just a toy model, which following https://arxiv.org/abs/1706.01159.
+
+File lists:
+ - generate_dataset.py : converting UCF-101 dataset to tfrecords.
+ - train.py : some train op.
+ - predict.py : using the trained model to predict the interpolated frames.
+ - metrics.py: include PSNR, SSIM and MS-SSIM to evaluate the quality of synthetic frame.
+ - ckpt_backup/* : some tained models.
+ - utils/* : some scripts may be useful.
+ 
--- predict.py
+++ predict.py
@@ -17,9 +17,9 @@ from metrics import PSNR, SSIM, MSSSIM
 model_path = "./ckpt_backup/"
 frame_path = "./test_video_frames/v_BalanceBeam_g05_c01/"
 
-frame1 = cv2.imread(frame_path+"1.png")
-frame2 = cv2.imread(frame_path+"2.png")
-frame3 = cv2.imread(frame_path+"3.png")
+frame1 = cv2.imread(frame_path+"7.png")
+frame2 = cv2.imread(frame_path+"8.png")
+frame3 = cv2.imread(frame_path+"9.png")
 
 input_test = np.concatenate((frame1, frame3), axis=2)
 input_test = np.array(input_test, dtype=np.float32)
@@ -63,17 +63,26 @@ with tf.Session() as sess:
 
     # print "loss = %f, PSNR = %f" % (loss, psnr)
 
-    plt.subplot(231)
+    plt.subplot(221)
+    plt.title("frame1")
     plt.imshow(frame1)
-    plt.subplot(232)
+    plt.imsave("results/frame1.png", frame1)
+    plt.subplot(222)
+    plt.title("frame3")
     plt.imshow(frame3)
+    plt.imsave("results/frame3.png", frame3)
 
-    plt.subplot(233)
+    plt.subplot(223)
+    plt.title("prediction")
     plt.imshow(prediction)
+    plt.imsave("results/prediction.png", prediction)
 
     # plt.subplot(234)
     # plt.imshow(avg_frame)
 
-    plt.subplot(234)
+    plt.subplot(224)
+    plt.title("ground truth")
     plt.imshow(frame2)
+    plt.imsave("results/frame2.png", frame2)
+    plt.suptitle("Loss = %.2f, PSNR = %.2f, SSIM = %.4f, MS-SSIM = %.4f" % (loss, psnr, ssim, ms_ssim))
     plt.show()
