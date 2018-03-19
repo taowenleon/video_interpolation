55a217eb5e127a478c678e3aa5804ba02f3597fb
taowen <taowen0828@gmail.com>
2018-03-19 09:40:23 +0800

    modified psnr

--- metrics.py
+++ metrics.py
@@ -82,9 +82,15 @@ def MSSSIM(img1, img2):
             (mssim[level - 1] ** weight[level - 1]))
 
 
-def PSNR(img1, img2):
-    mse = np.mean((img1 - img2) ** 2)
-    if mse == 0:
-        return 100
-    PIXEL_MAX = 255.0
-    return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
+def PSNR(target, ref):
+    # mse = np.mean((img1 - img2) ** 2)
+    # if mse == 0:
+    #     return 100
+    # PIXEL_MAX = 255.0
+    # return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
+    target_data = np.array(target, dtype=np.float64)
+    ref_data = np.array(ref,dtype=np.float64)
+    diff = ref_data - target_data
+    diff = diff.flatten('C')
+    rmse = math.sqrt(np.mean(diff ** 2.) )
+    return 20*math.log10(255.0/rmse)
